e5e93365269f242fe262211c429334b3af9d7590
Kim Sang Min <yhoo112@naver.com>
2020-08-12 22:11:43 +0900

    0812home

--- firstProject/src/com/yedam/Lotto/Lotto.java
+++ firstProject/src/com/yedam/Lotto/Lotto.java
@@ -1,21 +1,21 @@
-package com.yedam.rotto;
+package com.yedam.Lotto;
 
-public class test1 {
+public class Lotto {
 	public static void main(String[] args) {
 		int num1 =0, num2 = 0, num3 =0, num4 =0, num5 =0, num6 =0, num7 =0; //뽑힌 숫자
-		num1 = (int) (Math.random() * 46) + 1;
+		num1 = (int) (Math.random() * 45) + 1;
 				System.out.println(num1);
 		boolean AA = true;
 		while (AA) {
-		num2 = (int) (Math.random() * 46) + 1;
+		num2 = (int) (Math.random() * 45) + 1;
 			if (num2 != num1) {
 				System.out.println(num2);
 				AA = false;
-			} 
+			}
 		}//end of while
 		AA = true;
 		while (AA) {
-		num3 = (int) (Math.random() * 46) + 1;
+		num3 = (int) (Math.random() * 45) + 1;
 			if ((num3 != num1) && (num3 != num2)) {
 				System.out.println(num3);
 				AA = false;
@@ -23,7 +23,7 @@ public class test1 {
 		}//end of while
 		AA = true;
 		while (AA) {
-		num4 = (int) (Math.random() * 46) + 1;
+		num4 = (int) (Math.random() * 45) + 1;
 			if ((num4 != num1) && (num4 != num2) && (num4 != num3)) {
 				System.out.println(num4);
 				AA = false;
@@ -31,7 +31,7 @@ public class test1 {
 		}//end of while
 		AA = true;
 		while (AA) {
-		num5 = (int) (Math.random() * 46) + 1;
+		num5 = (int) (Math.random() * 45) + 1;
 			if ((num5 != num1) && (num5 != num2) && (num5 != num3) && (num5 != num4)) {
 				System.out.println(num5);
 				AA = false;
@@ -39,17 +39,9 @@ public class test1 {
 		}//end of while
 		AA = true;
 		while (AA) {
-		num6 = (int) (Math.random() * 46) + 1;
+		num6 = (int) (Math.random() * 45) + 1;
 			if ((num6 != num1) && (num6 != num2) && (num6 != num3) && (num6 != num4) && (num6 != num5)) {
 				System.out.println(num6);
-				AA = false;
-			} 
-		}//end of while
-		AA = true;
-		while (AA) {
-		num7 = (int) (Math.random() * 46) + 1;
-			if ((num7 != num1) && (num7 != num2) && (num7 != num3) && (num7 != num4) && (num7 != num5) && (num7 != num6)) {
-				System.out.println("보너스 번호:" + num7);
 				AA = false;
 			} 
 		}//end of while

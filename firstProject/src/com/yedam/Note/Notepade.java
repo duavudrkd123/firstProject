0751d89615754bafb66773071b2f06a4d53fb922
Kim Sang Min <yhoo112@naver.com>
2020-08-24 17:37:50 +0900

    0824class

--- firstProject/src/com/yedam/Note/Notepade.java
+++ firstProject/src/com/yedam/Note/Notepade.java
@@ -1,12 +1,10 @@
 package com.yedam.Note;
 
+
+
 public class Notepade {
-	public static void main(String[] args) {
-		for(int b = 2; b < 10; b++) {
-			int a=2;
-			for(a = 2; a <10; a++) {
-			 System.out.println(a + "*" + b + "=" + (a*b));	
-			}
-		}	
-	}
+
+		
+		
+	
 }
